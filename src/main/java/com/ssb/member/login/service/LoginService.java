dff1bc5f9e33eb7e52924e87dbe510dc19b14d88
srec0043 <srec0043@SSPN-SREC004301.KoreaZinc.co.kr>
2021-07-14 12:23:17 +0900

    다국어처리 및 VALIDATION 체크 로직 추가

--- src/main/java/com/ssb/member/login/service/LoginService.java
+++ src/main/java/com/ssb/member/login/service/LoginService.java
@@ -1,5 +1,11 @@
 package com.ssb.member.login.service;
 
+import javax.validation.Valid;
+
+import com.ssb.member.login.model.MeberVO;
+
 public interface LoginService {
+
+	MeberVO loginChk(MeberVO meberVo) throws Exception;
 
 }
