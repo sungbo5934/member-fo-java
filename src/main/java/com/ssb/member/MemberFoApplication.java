a5e93e230a4cded3ef4a1239d266bd96c0002a66
srec0043 <srec0043@SSPN-SREC004301.KoreaZinc.co.kr>
2021-09-02 10:25:51 +0900

    aop 적용하여 모든 request response에 log 쌓이게 적용

--- src/main/java/com/ssb/member/MemberFoApplication.java
+++ src/main/java/com/ssb/member/MemberFoApplication.java
@@ -5,10 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableCircuitBreaker
+@EnableAspectJAutoProxy
 @ComponentScan({"com.ssb.member.*","com.ssb.comm.*"})
 public class MemberFoApplication {
 
